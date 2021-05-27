ce0139bbd4192ac90d0b5e59bc4651c9b3142162
Konstantin-174 <timokhin_74@hotmail.com>
2021-05-27 23:02:17 +0500

    Updates

---
+++ precache-manifest.40ea0ef3f827c858135f32100b3e1718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f61a1e424b5c9ca881b2",
+    "url": "/sn-new/static/css/main.531c88a5.chunk.css"
+  },
+  {
+    "revision": "f61a1e424b5c9ca881b2",
+    "url": "/sn-new/static/js/main.d97c8a61.chunk.js"
+  },
+  {
+    "revision": "b7fa86622f94d51fcaa4",
+    "url": "/sn-new/static/js/runtime~main.0ab157f6.js"
+  },
+  {
+    "revision": "54edeba4588e816f815b",
+    "url": "/sn-new/static/js/2.5a70a557.chunk.js"
+  },
+  {
+    "revision": "eb838340751c0b5cf9352e559af645a1",
+    "url": "/sn-new/static/media/IRh4RWeX.eb838340.jpg"
+  },
+  {
+    "revision": "eb56b1a412b73a355d034c778104b06e",
+    "url": "/sn-new/static/media/ghML0mY_.eb56b1a4.jpg"
+  },
+  {
+    "revision": "f48d89a952b84baca6cd1ec228949793",
+    "url": "/sn-new/index.html"
+  }
+];
