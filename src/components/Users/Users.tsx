22298e0d8712cf462b3f45527315be4fbad75cda
Konstantin-174 <timokhin_74@hotmail.com>
2021-05-30 17:26:16 +0500

    33th (Refactoring)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -10,40 +10,17 @@ export const Users: React.FC<UsersPropsType> = ({
                                                     unfollow,
                                                     setUsers
                                                 }) => {
-    if (users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            setUsers(response.data.items)
-        })
-        // setUsers([
-        //     {
-        //         image: 'https://tridigital.wpengine.com/wp-content/uploads/2014/10/Turanga-Leela-Cartoon-Anime.jpg',
-        //         id: v1(),
-        //         followed: true,
-        //         status: 'Never give up!',
-        //         fullName: 'Turanga Leela',
-        //         location: {city: 'Old New York', country: 'USA'}
-        //     },
-        //     {
-        //         image: 'https://i.pinimg.com/736x/bb/4f/45/bb4f4529f8d7d3d60aa6fb3096b94ee6--futurama-why-not.jpg',
-        //         id: v1(),
-        //         followed: false,
-        //         status: 'Whoop! Whoop! Whoop! Whoop! Whoop!',
-        //         fullName: 'John D. Zoidberg',
-        //         location: {city: 'Noname', country: 'Decapod 10'}
-        //     },
-        //     {
-        //         image: 'http://oyster.ignimgs.com/mediawiki/apis.ign.com/futurama/d/d5/AmyWong.jpg',
-        //         id: v1(),
-        //         followed: false,
-        //         status: 'Ok Fry, we\'re done putting on the bra!',
-        //         fullName: 'Amy Wong',
-        //         location: {city: 'New York', country: 'USA'}
-        //     }
-        // ])
+    const getUsers = () => {
+        if (users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <section className={styles.usersWrap}>
+            <button onClick={getUsers} className={styles.getBtn}>Get Users</button>
             {
                 users.map(u => (
                     <section className={styles.innerWrap} key={u.id}>
