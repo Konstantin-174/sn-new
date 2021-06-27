4a635da85cf1d35aa0d69dc2c6eb91aa5d1c15b6
Konstantin-174 <timokhin_74@hotmail.com>
2021-06-27 14:13:28 +0500

    Updates

--- precache-manifest.d4af5cfaee60e008a94c1056b7dcea59.js
+++ precache-manifest.d4af5cfaee60e008a94c1056b7dcea59.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "5dac403b6f44ccdcfccf",
+    "revision": "51592263f199dbbc5ed6",
     "url": "/sn-new/static/css/main.757a9722.chunk.css"
   },
   {
-    "revision": "5dac403b6f44ccdcfccf",
-    "url": "/sn-new/static/js/main.27bed904.chunk.js"
+    "revision": "51592263f199dbbc5ed6",
+    "url": "/sn-new/static/js/main.13782dc2.chunk.js"
   },
   {
     "revision": "b7fa86622f94d51fcaa4",
     "url": "/sn-new/static/js/runtime~main.0ab157f6.js"
   },
   {
-    "revision": "88dbe4d01f7d167dd756",
-    "url": "/sn-new/static/js/2.4071af23.chunk.js"
+    "revision": "13450afca477221c066b",
+    "url": "/sn-new/static/js/2.20c904d3.chunk.js"
   },
   {
     "revision": "2a09aa4b34a10eeb3be35978e8d33087",
     "url": "/sn-new/static/media/nonameUser.2a09aa4b.jpg"
   },
   {
-    "revision": "285ebce3aa9d25cb493f2942ec4f2817",
+    "revision": "2f05d540ae36ef4e8f47f96696d54196",
     "url": "/sn-new/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sn-new/precache-manifest.455d74455702b02619266ecd4d255271.js"
+  "/sn-new/precache-manifest.d4af5cfaee60e008a94c1056b7dcea59.js"
 );
 
 workbox.clientsClaim();
